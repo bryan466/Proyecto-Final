0613af6cb55a8bd04c4f15dd268058fe373e938d
bryan466 <73204227+bryan466@users.noreply.github.com>
2021-01-30 01:41:43 -0700

    Add files via upload

---
+++ graficas.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+                Peliculas Bj
+        </title>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <link rel='stylesheet' type='text/css'  href='css/estilos.css?v=0.0.81'/>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    </head>
+    <body>
+            <div class="fondo-peliculas">
+                    <div class="menu-lateral">
+                                <div class="logo2">
+                                <img src="img/icon (1).png"  height="50px">  
+                                </div>
+                                <br>
+                                <div class="name2"> <a   href="#">Peliculas Bj</a></div><br>
+                                <div  class="lista-menu">
+                                    <ul class="lista-menu-1">
+                                        <strong><li><a href="index.php"  style="color:#FF0000;" >Inicio</a></li></strong><br>
+                                        <strong><li><a href="peliculas.php"  style="color:#FF0000;">Configurar peliculas</a></li></strong><br>
+                                        <strong><li><a href="categories.php"  style="color:#FF0000;">Configurar categorias</a></li></strong><br>
+                                        <strong><li><a href="login.php"  style="color:#FF0000;" >Inciar sesion</a></li></strong><br>
+                                        <strong><li><a href="graficas.php"  style="color:#FF0000;">Graficas</a></li></strong>
+                                    </ul>
+                                </div>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                        </div>
+
+            </div>
+</body>
+</html>
